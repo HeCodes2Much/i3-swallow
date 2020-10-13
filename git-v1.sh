357ce33cdf64f3b8894a506e2ddb6eedb351c85e
TheCynicalLiger <wayne6324@gmail.com>
2020-10-13 16:09:33 +0100

    Update

---
+++ git-v1.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+##################################################################################################################
+# Author 	: 	TheCynicalLiger
+# Website	:	https://github.com/TheCynicalLiger/
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+# checking if I have the latest files from github
+echo "Checking for newer files online first"
+git pull
+
+# Below command will backup everything inside the project folder
+git add --all .
+
+# Give a comment to the commit if you want
+echo "####################################"
+echo "Write your commit comment!"
+echo "####################################"
+
+read input
+
+# Committing to the local repository with a message containing the time details and commit text
+
+git commit -m "$input"
+
+# Push the local files to github
+
+git push -u origin main
+
+
+echo "################################################################"
+echo "###################    Git Push Done      ######################"
+echo "################################################################"
---
+++ setup-git-v1.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+##################################################################################################################
+# Author 	: 	TheCynicalLiger
+# Website	:	https://github.com/TheCynicalLiger/
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+# Problem solving commands
+
+# Read before using it.
+# https://www.atlassian.com/git/tutorials/undoing-changes/git-reset
+# git reset --hard orgin/master
+# ONLY if you are very sure and no coworkers are on your github.
+
+# Command that have helped in the past
+# Force git to overwrite local files on pull - no merge
+# git fetch all
+# git push --set-upstream origin master
+# git reset --hard orgin/master
+
+#setting up git
+#https://www.atlassian.com/git/tutorials/setting-up-a-repository/git-config
+
+git init
+git config --global user.name " TheCynicalLiger"
+git config --global user.email "wayne6324@gmail.com"
+sudo git config --system core.editor nano
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=25000'
+git config --global push.default simple
+git config pull.rebase false  # merge (the default strategy)
+git config pull.ff only       # fast-forward only
+
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
